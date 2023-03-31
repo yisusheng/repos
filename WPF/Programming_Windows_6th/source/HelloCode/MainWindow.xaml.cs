b7e1b77eb71c0bc868d5f57ba66ca5f8581f1913
yisusheng <yisusheng666@gmail.com>
2023-03-31 19:56:23 +0800

    add HelloCode

---
+++ WPF/Programming_Windows_6th/source/HelloCode/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace HelloCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TextBlock txtblk = new TextBlock();
+            txtblk.Text = "Hello,WPF!";
+            txtblk.FontFamily = new System.Windows.Media.FontFamily("Times New Roman");
+            txtblk.FontSize = 96;
+            txtblk.Foreground = new SolidColorBrush(Colors.Yellow);
+            txtblk.HorizontalAlignment = HorizontalAlignment.Center;
+            txtblk.VerticalAlignment = VerticalAlignment.Center;
+
+            txtblk.FontStyle = FontStyles.Italic;
+
+            contentGrid.Children.Add(txtblk);
+        }
+    }
+}
