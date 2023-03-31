04381149df07ab740415256543f0d7a992539950
yisusheng <yisusheng666@gmail.com>
2023-03-31 20:40:56 +0800

    add StrippedDownHello

---
+++ WPF/Programming_Windows_6th/source/StrippedDownHello/App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StrippedDownHello
+{
+    public class App:Application
+    {
+        static void Main(string[] args)
+        {
+            StrippedDownHello.App app = new StrippedDownHello.App();
+            app.Run();
+        }
+    }
+}
