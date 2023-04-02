01a1283a7b3e2aee92d4ba6ee8a356ebfbbb103a
yisusheng <yisusheng666@gmail.com>
2023-04-02 13:51:58 +0800

    add ImageBrushedSpiral

---
+++ WPF/Programming_Windows_6th/source/ImageBrushedSpiral/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageBrushedSpiral
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int angle = 0; angle < 3600; angle++)
+            {
+                double radians = Math.PI * angle / 180;
+                double radius = angle / 10;
+                double x = 360 + radius * Math.Sin(radians);
+                double y = 360 - radius * Math.Cos(radians);
+                polyline.Points.Add(new Point(x, y));
+            }
+        }
+    }
+}
