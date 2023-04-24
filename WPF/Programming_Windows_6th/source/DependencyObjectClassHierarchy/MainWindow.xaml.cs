f563437fda6ff1ad7159787a90f2a40dd2c36bdf
yisusheng <yisusheng666@gmail.com>
2023-04-24 10:56:34 +0800

    update

---
+++ WPF/Programming_Windows_6th/source/DependencyObjectClassHierarchy/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DependencyObjectClassHierarchy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Type rootType = typeof(DependencyObject);
+        TypeInfo rootTypeInfo = typeof(DependencyObject).GetTypeInfo();
+        List<Type> classes = new List<Type>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddToClassList(typeof(System.Windows.DependencyObject));
+        }
+
+        void AddToClassList(Type sampleType)
+        {
+
+        }
+    }
+
+    
+}
