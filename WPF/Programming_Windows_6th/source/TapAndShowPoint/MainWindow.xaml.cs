dbc6020eb3692d5902600f256ad70e632f4086e0
yisusheng <yisusheng666@gmail.com>
2023-04-27 16:03:18 +0800

    update

---
+++ WPF/Programming_Windows_6th/source/TapAndShowPoint/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TapAndShowPoint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            Point pt = e.GetPosition(this);
+
+            Ellipse ellipse = new()
+            {
+                Width = 3,
+                Height = 3,
+                Fill = this.Foreground
+            };
+
+            Canvas.SetLeft(ellipse, pt.X);
+            Canvas.SetTop(ellipse, pt.Y);
+            canvas.Children.Add(ellipse);
+
+            TextBlock textBlock = new()
+            {
+                Text = String.Format("({0})", pt),
+                FontSize = 24
+            };
+
+            Canvas.SetLeft(textBlock, pt.X);
+            Canvas.SetTop(textBlock, pt.Y);
+            canvas.Children.Add(textBlock);
+
+            e.Handled = true;
+            base.OnMouseLeftButtonUp(e);
+        }
+    }
+}
