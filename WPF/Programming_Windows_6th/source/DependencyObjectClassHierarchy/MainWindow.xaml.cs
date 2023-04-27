8d95c7f2e0a18b970356f1f4aefebbfb3944c7b8
yisusheng <yisusheng666@gmail.com>
2023-04-27 11:57:38 +0800

    update

--- WPF/Programming_Windows_6th/source/DependencyObjectClassHierarchy/MainWindow.xaml.cs
+++ WPF/Programming_Windows_6th/source/DependencyObjectClassHierarchy/MainWindow.xaml.cs
@@ -21,20 +21,82 @@ namespace DependencyObjectClassHierarchy
     /// </summary>
     public partial class MainWindow : Window
     {
-        Type rootType = typeof(DependencyObject);
-        TypeInfo rootTypeInfo = typeof(DependencyObject).GetTypeInfo();
-        List<Type> classes = new List<Type>();
+        private Type rootType = typeof(DependencyObject);
+        private TypeInfo rootTypeInfo = typeof(DependencyObject).GetTypeInfo();
+        private List<Type> classes = new List<Type>();
+
         public MainWindow()
         {
             InitializeComponent();
             AddToClassList(typeof(System.Windows.DependencyObject));
+            classes.Sort((t1, t2) =>
+            {
+                return String.Compare(t1.GetTypeInfo().Name, t2.GetTypeInfo().Name);
+            });
+            var rootClass = new ClassAndSubclasses(rootType);
+            AddToTree(rootClass, classes);
+            Display(rootClass, 0);
         }
 
-        void AddToClassList(Type sampleType)
+        private void AddToClassList(Type sampleType)
         {
+            var assembly = sampleType.GetTypeInfo().Assembly;
+            foreach (var type in assembly.ExportedTypes)
+            {
+                TypeInfo typeInfo = type.GetTypeInfo();
+                if (typeInfo.IsPublic && rootTypeInfo.IsAssignableFrom(typeInfo))
+                    classes.Add(type);
+            }
+        }
 
+        private void AddToTree(ClassAndSubclasses parentClass, List<Type> classes)
+        {
+            foreach (var type in classes)
+            {
+                var baseType = type.GetTypeInfo().BaseType;
+
+                if (baseType == parentClass.Type)
+                {
+                    var subClass = new ClassAndSubclasses(type);
+                    parentClass.Subclasses.Add(subClass);
+                    AddToTree(subClass, classes);
+                }
+            }
+        }
+
+        private void Display(ClassAndSubclasses parentClass, int indent)
+        {
+            var typeInfo = parentClass.Type.GetTypeInfo();
+
+            TextBlock txtblk = new();
+            txtblk.Inlines.Add(new Run { Text = new string(' ', 8 * indent) });
+            txtblk.Inlines.Add(new Run { Text = typeInfo.Name });
+
+            if (typeInfo.IsSealed)
+                txtblk.Inlines.Add(new Run
+                {
+                    Text = " (sealed)"
+                });
+
+            IEnumerable<ConstructorInfo> constructorInfos = typeInfo.DeclaredConstructors;
+            int publicConstructorCount = 0;
+
+            foreach (var constructorInfo in constructorInfos)
+            {
+                if (constructorInfo.IsPublic)
+                    publicConstructorCount += 1;
+            }
+
+            if (publicConstructorCount == 0)
+                txtblk.Inlines.Add(new Run
+                {
+                    Text = " (non-instantiable)"
+                });
+
+            stackPanel.Children.Add(txtblk);
+
+            foreach (var subclass in parentClass.Subclasses)
+                Display(subclass, indent + 1);
         }
     }
-
-    
 }
