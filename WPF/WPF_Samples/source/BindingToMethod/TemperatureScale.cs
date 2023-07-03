d0b36af77907619f50dfeaf4e4f492b3bb28d99f
yisusheng <yisusheng666@gmail.com>
2023-07-03 14:25:09 +0800

    BindingToMethod

---
+++ WPF/WPF_Samples/source/BindingToMethod/TemperatureScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingToMethod
+{
+    public class TemperatureScale : INotifyPropertyChanged
+    {
+        private TempType _type;
+
+        public TemperatureScale()
+        { }
+
+        public TemperatureScale(TempType type)
+        {
+            _type = type;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
