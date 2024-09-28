8a67a4d0701f7e1e61d623d958274372cdb06ca0
yisusheng <yisusheng666@gmail.com>
2024-09-28 20:59:15 +0800

    更新

---
+++ learn/C#/WPF/WPF/TestCAD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+
+namespace WPF
+{
+    public class TestCAD
+    {
+        [CommandMethod("testcad")]
+        public void MainTest()
+        {
+            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("成功打开");
+            var window = new MainWindow();
+            Application.ShowModelessWindow(window);
+        }
+    }
+}
